8136c959432a36b68277722f672b17c6f3968a6e
Rubem Kalebe <rubemkalebe@hotmail.com>
2017-04-14 12:29:56 -0300

    Extract data module implemented

---
+++ src/data/extract_data.py
@@ -0,0 +1,17 @@
+'''
+Created on 14 de abr de 2017
+
+@author: rubemkalebe
+'''
+
+import pandas
+
+class ExtractData(object):
+    '''
+    Extract data from Natal (Brazil) City Hall (http://ckan.imd.ufrn.br/) 
+    '''
+
+    @staticmethod
+    def read_csv(url): 
+        data = pandas.read_csv(url, sep = ';')
+        return data 
